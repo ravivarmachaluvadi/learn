fc8aec5d18ce13eb3c2fb6789ddb363de8c58477
Ravi Varma Chaluvadi <58871650+ravivarmachaluvadi@users.noreply.github.com>
2022-08-15 14:48:09 +0530

    dummy changes

--- src/main/kotlin/com/kotlin/learn/Comment.java
+++ src/main/kotlin/com/kotlin/learn/Comment.java
@@ -2,6 +2,7 @@ package com.kotlin.learn;
 
 public class Comment {
     /*
+    
     …or create a new repository on the command line
  echo "# learn" >> README.md
 
@@ -11,7 +12,6 @@ git commit -m "first commit"
 git branch -M main
 git remote add origin git@github.com:ravivarmachaluvadi/learn.git
 git push -u origin main
-
 
 …or push an existing repository from the command line
  git remote add origin git@github.com:ravivarmachaluvadi/learn.git
