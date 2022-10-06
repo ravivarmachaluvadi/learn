2cbffb10fbc67648d8c2a929252a5e9f41f0aa87
ravivarmachaluvadi <ravivarmachaluvadi@gmail.com>
2022-10-06 20:53:50 +0530

    10 Oct 9PM

---
+++ src/main/kotlin/com/kotlin/learn/Person.kt
@@ -0,0 +1,32 @@
+package com.kotlin.learn
+
+//PROVIDING DEFAULT VALUES
+class Person(val firstName: String = "Ravi", val lastName: String = "Varma") {
+
+    //MUTABLE
+    var nickName: String? = null
+
+    init {
+        println("Init 1")
+    }
+
+    init {
+        println("Init 2")
+    }
+
+    constructor() : this("Varma", "Ravi") {
+        println("Secondary Construtor")
+    }
+
+    override fun toString(): String {
+        //READING STRING VALUES USING $
+        return "Person(firstName='$firstName', lastName='$lastName', nickName=$nickName)"
+    }
+
+    fun printInfo() {
+        //ELVIS OPERATOR
+        val nickNameToPrint = nickName ?: "no nickName"
+        println("$firstName ($nickNameToPrint) $lastName")
+    }
+
+}
---
+++ src/main/kotlin/com/kotlin/learn/VarArgDemo.kt
@@ -0,0 +1,35 @@
+import com.kotlin.learn.Person
+
+fun sayHello(name: String, vararg lastName: String) {
+ lastName.forEach { lastName ->
+ println("$name $lastName")
+
+ }
+ }
+
+ fun sayHello1(name: String, lastName: String,surName: String = "Chaluvadi") {
+ println("$name $lastName $surName")
+ }
+
+ //SINGLE LINE FUNCTION EXPRESSION
+ fun getHello() : String = "G81B"
+
+ fun main() {
+
+ sayHello("Ravi")
+
+ //VAL MEANS IMMUTABLE
+ val listOf = arrayOf("Varma", "Chaluvadi")
+
+ //* SPREAD OPERATOR CALL FOR LIST TYPE
+ sayHello("Ravi", *listOf)
+ println(getHello())
+
+ //UNORDERED AND NAMED ARGS CALL
+ sayHello1(lastName = "Varma",name="Ravi")
+
+ val person= Person()
+
+ person.printInfo()
+
+ }
